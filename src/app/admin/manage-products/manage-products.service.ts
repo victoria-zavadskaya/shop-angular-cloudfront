21922f6ddfa61f74e6cca80367c1a7286fbef092
Viktoryia Zavatskaya <viktoryia_zavatskaya@epam.com>
2023-11-17 22:44:13 +0300

    feat: add authorization

--- src/app/admin/manage-products/manage-products.service.ts
+++ src/app/admin/manage-products/manage-products.service.ts
@@ -6,37 +6,36 @@ import { ApiService } from '../../core/api.service';
 
 @Injectable()
 export class ManageProductsService extends ApiService {
-  constructor(injector: Injector) {
-    super(injector);
-  }
-
-  uploadProductsCSV(file: File): Observable<unknown> {
-    if (!this.endpointEnabled('import')) {
-      console.warn(
-        'Endpoint "import" is disabled. To enable change your environment.ts config'
-      );
-      return EMPTY;
+    constructor(injector: Injector) {
+        super(injector);
     }
 
-    return this.getPreSignedUrl(file.name).pipe(
-      switchMap((url) =>
-        this.http.put(url, file, {
-          headers: {
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            'Content-Type': 'text/csv',
-          },
-        })
-      )
-    );
-  }
+    public uploadProductsCSV(file: File): Observable<unknown> {
+        if (!this.endpointEnabled('import')) {
+            console.warn('Endpoint "import" is disabled. To enable change your environment.ts config');
+            return EMPTY;
+        }
 
-  private getPreSignedUrl(fileName: string): Observable<string> {
-    const url = this.getUrl('import', 'import');
+        return this.getPreSignedUrl(file.name).pipe(
+            // switchMap((url) =>
+            //     this.http.put(url, file, {
+            //         headers: { 'Content-Type': 'text/csv' },
+            //     })
+            // )
+        );
+    }
 
-    return this.http.get<string>(url, {
-      params: {
-        name: fileName,
-      },
-    });
-  }
+    private getPreSignedUrl(fileName: string): Observable<any> {
+        const url = this.getUrl('import', 'import');
+        const token = localStorage.getItem('authorizationToken');
+
+        return this.http.get(url, {
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Authorization': `Basic ${token}`,
+            },
+            params: { name: fileName },
+            responseType: 'text',
+        });
+    }
 }
